f5beef55ead210099d125a0d6817066fc63ba6bd
Paloma Alves <pa.oliveiraalves556@gmail.com>
2023-10-17 15:05:19 -0300

    Solved challange from codewars

--- level_08/02_convert_boolean_values .rb
+++
@@ -1,9 +0,0 @@
-# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
-
-def bool_to_word bool
-  if bool == true
-    return 'Yes'
-  else
-    return 'No'
-  end
-end
